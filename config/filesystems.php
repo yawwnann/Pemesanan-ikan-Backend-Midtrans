17f133e14dce551e26d3b64296a31af8b4a947da
yawwnann <c0478592@gmail.com>
2025-05-04 11:12:54 +0700

    config

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,10 +32,8 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app/private'),
-            'serve' => true,
+            'root' => storage_path('app'), // <-- Pastikan ini benar
             'throw' => false,
-            'report' => false,
         ],
 
         'public' => [
